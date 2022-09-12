119ecd4480fe7cec4ad2afe9a09f7e5c0432f946
raviw-2022 <ravi.s@webkorps.com>
2022-09-13 01:53:26 +0530

    Install Factory_Bot Gem

--- config/application.rb
+++ config/application.rb
@@ -19,13 +19,7 @@ module RspecDemoTest
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
-      g.test_framework (
-        :rspec,
-        fixtures: false,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-      )
+      g.test_framework = :rspec
     end
   end
 end
